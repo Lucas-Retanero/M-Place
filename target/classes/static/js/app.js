7b62bf06c8108eba0deffa99ac89de9518a12ba0
Lucas-Retanero <glucas.raro@gmail.com>
2025-04-19 01:44:03 -0300

    Implementação do front com o Vue

---
+++ target/classes/static/js/app.js
@@ -0,0 +1,22 @@
+const app = {
+    data() {
+        //variáveis
+        return {
+
+        };
+    },
+    //métodos
+    methods: {
+
+    },
+    //templates 
+    template: `
+     
+     <h1>Vue funcionando!</h1>
+	 <h1>:)</h1>
+	
+    `
+};
+
+const App = Vue.createApp(app);
+App.mount('#app');
