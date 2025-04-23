dbc83d87b4785f9470f9d74997f65fc667512351
Lucas-Retanero <glucas.raro@gmail.com>
2025-04-22 22:58:06 -0300

    Card de brinquedo finalizado

--- src/main/resources/static/js/components/home.js
+++ src/main/resources/static/js/components/home.js
@@ -6,26 +6,26 @@ export const Home = {
         
     },
     template: `
-    <section class="home">
-		<h1 id="titulo-home">Brinquedos em Destaque</h1>
+    
+	<section class="home">
+			<h1 id="titulo-home">Brinquedos em Destaque</h1>
 
-		<div class="destaques">
+			<div class="destaques">
 
-			<div class="card-brinquedo">
-				<div class="img-brinquedo">
-					<img src="https://superlegalbrinquedos.vtexassets.com/arquivos/ids/228379-800-auto?v=638429965434500000&width=800&height=auto&aspect=true" alt="Brinquedo 1" title="Brinquedo 1">
+				<div class="card-brinquedo">
+					<div class="img-brinquedo">
+						<img src="https://superlegalbrinquedos.vtexassets.com/arquivos/ids/228379-800-auto?v=638429965434500000&width=800&height=auto&aspect=true" alt="Brinquedo 1" title="Brinquedo 1">
+					</div>
+					<p id="descricao-brinquedo">LEGO Minecraft - Expedição do Steve ao Deserto - 75 Peças - 21251</p>
+					<div class="preco-brinquedo">
+						<p id="dinheiro">R$ 99,99</p>
+						<p id="pix">R$ 89,99 no PIX</p>
+					</div>
+					<button id="btn-comprar">Comprar</button>
 				</div>
-				<div class="descricao-brinquedo">
-					<p>LEGO Minecraft - Expedição do Steve ao Deserto - 75 Peças - 21251</p>
-				</div>
-				<div class="preco-brinquedo">
-					<p id="dinheiro">R$ 99,99</p>
-					<p id="pix">R$ 89,99 no PIX</p>
-				</div>
-				<button id="btn-comprar">Comprar</button>
+
 			</div>
-
-		</div>
 	</section>
+
     `,
 };
