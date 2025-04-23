34e45b3ba7715087a728fc4ed92b1ff4e1c7b4a3
Kevyn-Cavalcanti <contacontaconta0002@gmail.com>
2025-04-23 19:41:41 -0300

    router fix

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -1,71 +1,60 @@
-// import { Home } from './components/home.js';
-// import { Catalogo } from './components/catalogo.js';
-// import { Administracao } from './components/administacao.js';
-// import { Equipe } from './components/equipe.js';
+import { Home } from './components/home.js';
+import { Catalogo } from './components/catalogo.js';
+import { Administracao } from './components/administacao.js';
+import { Equipe } from './components/equipe.js';
 
-// const routes = [
-//     { path: '/', component: Home },
-//     { path: '/catalogo', component: Catalogo },
-//     { path: '/admin', component: Adiministracao },
-//     { path: '/equipe', component: Equipe },
-// ];
+const routes = [
+    { path: '/', component: Home },
+    { path: '/catalogo', component: Catalogo },
+    { path: '/admin', component: Administracao },
+    { path: '/equipe', component: Equipe },
+];
 
-// const router = VueRouter.createRouter({
-//     history: VueRouter.createWebHashHistory(),
-//     routes
-// });
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes
+});
 
 const app = {
     data() {
-        //variáveis
         return {
-
         };
     },
-    //métodos
     methods: {
-
     },
-    //templates 
     template: `
-     
-    <nav>
-		<ul>
-			<li id="logo">
-				<i id="logo" class="fi fi-sc-gamepad"></i>
-			</li>
-			<li>
-				<i class="fi fi-sr-home"></i>
-				<p>Home</p>
-			</li>
-			<li>
-				<i class="fi fi-sr-catalog"></i>
-				<p>Catálogo</p>
-			</li>
-			<li>
-				<i class="fi fi-sr-file-edit"></i>
-				<p>Adiministração</p>
-			</li>
-			<li>
-				<i class="fi fi-sr-users"></i>
-				<p>Equipe</p>
-			</li>
-		</ul>
-	</nav>
+        <nav>
+            <ul>
+                <li id="logo">
+                    <i id="logo" class="fi fi-sc-gamepad"></i>
+                </li>
+                <li>
+                    <router-link to="/"><i class="fi fi-sr-home"></i><p>Home</p></router-link>
+                </li>
+                <li>
+                    <router-link to="/catalogo"><i class="fi fi-sr-catalog"></i><p>Catálogo</p></router-link>
+                </li>
+                <li>
+                    <router-link to="/admin"><i class="fi fi-sr-file-edit"></i><p>Administração</p></router-link>
+                </li>
+                <li>
+                    <router-link to="/equipe"><i class="fi fi-sr-users"></i><p>Equipe</p></router-link>
+                </li>
+            </ul>
+        </nav>
 
-	<div class="container">
-		<header id="titulo">
-			<h1>M-Place</h1>
-		</header>
+        <div class="container">
+            <header id="titulo">
+                <h1>M-Place</h1>
+            </header>
 
-		<main>
-			<router-view></router-view>
-		</main>
-	</div>
-	
+            <main>
+                <router-view></router-view>
+            </main>
+        </div>
     `
 };
 
 const App = Vue.createApp(app);
-//App.use(router);
+App.use(router);
 App.mount('#app');
